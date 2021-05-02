3e75256bb6201177e36edf6ff4f5ddc3f33237e7
sunyu-pang <pangsunyu@gmail.com>
2021-05-02 21:47:56 +0900

    fixed ver.

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-let addButton = document.querySelector(".add_button");
-addButton.addEventListner("click", addInput);
+let todoForm = document.querySelector('.todo_form');
+let addButton = document.getElementById('add_button');
+let todoInput = document.getElementById('todo_input');
+let todoList = document.getElementById('todo_list');
 
 function deleteTodo(event){
-    let deleteli = event.target.parentNode;
+    let target = event.target;
+    let deleteli = target.parentNode;
     todoList.removeChild(deleteli);
 }
 
 function toggleTodo(event){
-    event.target.todoList.toggle('done');
+    let target = event.target;
+    target.style.textDecoration = "line-through";
 }
 
 function addInput(){
-    let todo = document.querySelector(".todo_input").value;
-    
-    if (todo != null){
-        let todoList = document.createElement("li");
+    if (todoInput.value != null){
+        let list = document.createElement("li");
+
         let deleteButton = document.createElement("button");
         deleteButton.innerText = "X";
-        deleteButton.name
         deleteButton.className = "delete_button";
+
         let span = document.createElement("span");
-        span.innerText = todo;
-        todoList.appendChild(span);
-        todoList.appendChild(deleteButton);
-        deleteButton.addEventListener("click", deleteTodo);
-        span.addEventListener("click", toggleTodo);
-        document.querySelector(".todo_input").value = "";
+        span.innerText = todoInput.value;
+        list.appendChild(span);
+        list.appendChild(deleteButton);
+        todoList.appendChild(list);
+
+        deleteButton.addEventListener('click', deleteTodo);
+        span.addEventListener('click', toggleTodo);
+
+        todoInput.value = "";
     }
 }
 
@@ -35,3 +41,8 @@ function addHandler(){
         addInput();
     }
 }
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addInput();
+});
