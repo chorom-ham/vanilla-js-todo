60236862c683c580d745c0bbc34b5b1781e799db
arm-B <arm-B@users.noreply.github.com>
2021-05-03 16:47:44 +0900

    revise Add to ul

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function deleteList(EVENT) {}
+var button = document.getElementById("enter");
+var input_data = document.getElementById("input_text");
+var ul = document.querySelector("ul");
 
-function lineList() {}
+function deleteList() {}
 
 function clickInput() {
-  const input_text = document.getElementById("input_text").value;
-  const li = document.createElement("li");
-  const textNode = document.createTextNode(input_text);
-  const delBtn = document.createElement("button");
-  delBtn.innerText = "X";
-  delBtn.addEventListener("click", deleteList);
-  textNode.addEventListener("click", lineList);
-  li.setAttribute("id", input_text);
-  li.appendChild(textNode);
-  li.appendChild(delBtn);
-  document.getElementById("task-list").appendChild(li);
+  const input_text = input_data.value;
+  if (input_text.length > 0) {
+    var li = document.createElement("li");
+    const textNode = document.createTextNode(input_text);
+    var btn = document.createElement("button");
+    btn.innerText = "X";
+    btn.addEventListener("click", deleteList);
+
+    li.appendChild(textNode);
+    li.innerHTML = li.innerHTML + " ";
+    li.appendChild(btn);
+    ul.appendChild(li);
+
+    input_data.value = "";
+  }
 }
+
+button.addEventListener("click", clickInput);
